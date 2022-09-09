654cfab1109a276c42739e13501ce4a833b9fd8d
rickytann14 <74378153+rickytann14@users.noreply.github.com>
2022-09-09 09:05:33 -0500

    Uploading second assignment

---
+++ CSCE-2100-Computing-Foundations-1/Spring2018/homework2/main.cpp
@@ -0,0 +1,36 @@
+#include "prgm.h"
+
+
+int main() {
+	population ppls;
+	int suscpet, infect, recover;
+	int vacc, trans_prob;
+	int contact, sims, infper;
+	
+	/*vector<person> pls;
+	vector<infected> inf;
+	vector<susceptible> sups;
+	vector<recovered> recov;*/
+
+	student();
+	ppls.add_data();
+	/*cout << "Please enter number of Susceptibles: ";
+	cin >> suscpet;
+	cout << "Please enter number of Infectious: ";
+	cin >> infect;
+	cout << "Please enter number of Recovered: ";
+	cin >> recover;
+	cout << "Please enter number of Vaccinated: ";
+	cin >> vacc;
+	cout << "Please enter contact rate: ";
+	cin >> contact;
+	cout << "Enter Transmission Probability: ";
+	cin >> trans_prob;
+	cout << "Enter Infectious Period: ";
+	cin >> infper;
+	cout << "Enter  number of Simulations: ";
+	cin >> sims;*/
+	
+	
+	return 0;
+}
